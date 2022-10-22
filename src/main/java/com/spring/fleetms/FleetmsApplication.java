0a34712fd98c8848ff603bc0e5c9de3655630c24
Willian-code <willianrosendodelima@outlook.com>
2022-10-22 20:43:46 -0300

    first commit

---
+++ src/main/java/com/spring/fleetms/FleetmsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.fleetms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetmsApplication.class, args);
+	}
+
+}
